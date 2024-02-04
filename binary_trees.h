428ded0c0be5230742dae01f59af530f09cc40ed
NiniolaX <loveafinni16@gmail.com>
2024-02-04 17:39:26 +0100

    Wrote header file

---
+++ binary_trees.h
@@ -0,0 +1,24 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left node
+ * @right: Pointer to the right node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif
