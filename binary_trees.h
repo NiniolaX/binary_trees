333a7b51aa3f5e39dc9f7750d2f3b197cca9f25d
NiniolaX <loveafinni16@gmail.com>
2024-02-07 13:09:40 +0100

    Wrote script thatcreates a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,5 +22,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
