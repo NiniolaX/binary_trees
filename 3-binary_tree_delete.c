f0510d90c1444e41a2d5685db1b6d9e5e04f2b53
NiniolaX <loveafinni16@gmail.com>
2024-02-08 11:40:40 +0100

    Refactored codebase of function that deletes a binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-void binary_tree_delete_helper(binary_tree_t *subtree);
-
 /**
  * binary_tree_delete - Deletes an entire binary tree
  * @tree: Pointer to root node of tree to delete
@@ -11,26 +9,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree)
 	{
-		binary_tree_delete_helper(tree);
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
 		free(tree);
-	}
-}
-
-/**
- * binary_tree_delete_helper - Deletes a subtree of a binary tree
- * @subtree: Pointer to subtree to be deleted
- * Return: Nothing
- */
-void binary_tree_delete_helper(binary_tree_t *subtree)
-{
-	if (subtree->left)
-	{
-		binary_tree_delete_helper(subtree->left);
-		free(subtree->left);
-	}
-	if (subtree->right)
-	{
-		binary_tree_delete_helper(subtree->right);
-		free(subtree->right);
 	}
 }
