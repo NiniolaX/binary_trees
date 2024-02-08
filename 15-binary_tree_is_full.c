a919c3842165fbbad728f6f4dececa08a0efe8a0
NiniolaX <loveafinni16@gmail.com>
2024-02-08 23:30:50 +0100

    Updated function to correctly handle trees not full and with height > 3

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,21 +7,18 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left_is_full = 0, right_is_full = 0;
+	int left_is_full, right_is_full;
 
 	if (tree == NULL)
 		return (0);
 
+	/* Trees with 0 children (i.e. leaves) are full */
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left != NULL && tree->right == NULL)
-		return (1);
-
+	/* Check if both left and right subtrees are full */
 	left_is_full = binary_tree_is_full(tree->left);
 	right_is_full = binary_tree_is_full(tree->right);
 
-	if (left_is_full == 1 && right_is_full == 1)
-		return (1);
-	else
-		return (0);
+	/* Bool: If expr is true, 1 is returned, if false, 0 is returned */
+	return (left_is_full && right_is_full);
 }
